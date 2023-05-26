3aeb2ded2f7e3d4642406325a62b0ffde773329f
jayant <kashiv03jayant@gmail.com>
2023-05-27 00:07:14 +0530

    created java project

---
+++ FirstDemo/src/demo/FirsrProgram.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class FirsrProgram {
+
+	public static void main(String[] args) {
+
+		        int num = 100, sum = 0;
+
+		        for(int i = 1; i <= num; ++i)
+		        {
+		            // sum = sum + i;
+		            sum += i;
+		        }
+
+		        System.out.println("Sum = " + sum);
+	}
+
+}
